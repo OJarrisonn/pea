9343ae65f52c9bc456c6c2d06214adc7ebec31b6
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-08-22 14:15:00 -0300

    feat: working ersion with custom shell support
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

---
+++ src/config.rs
@@ -0,0 +1,62 @@
+use std::error::Error;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    pub shell: String,
+    pub shell_args: Vec<String>,
+    pub pager: Option<String>,
+}
+
+impl Config {
+    pub fn load() -> Result<Self, Box<dyn Error>> {
+        let config_path = dirs::config_dir()
+            .ok_or(String::from("Failed to get config directory"))?
+            .join("pea")
+            .join("config.toml");
+
+        if config_path.exists() {
+            let config = std::fs::read_to_string(config_path)?;
+            Ok(toml::from_str::<Self>(&config).map_err(Box::new)?)
+        } else {
+            let config = Self::default();
+            let content = toml::to_string(&config)?;
+            std::fs::create_dir_all(config_path.parent().unwrap())?;
+            std::fs::write(config_path, content)?;
+            Ok(config)
+        }
+    }
+
+    pub fn with_pager(self) -> Self {
+        match &self.pager {
+            Some(_) => self,
+            None => Self {
+                pager: std::env::var("PEA_PAGER")
+                    .unwrap_or_else(|_| {
+                        std::env::var("PAGER").unwrap_or_else(|_| "less".to_string())
+                    })
+                    .into(),
+                ..self
+            },
+        }
+    }
+
+    pub fn format_shell_args(&mut self, command: &str) {
+        for arg in &mut self.shell_args {
+            if arg.is_empty() {
+                *arg = command.into();
+            }    
+        }
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            shell: "sh".to_string(),
+            shell_args: vec!["-c".into(), "{}".into()],
+            pager: None,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use std::collections::LinkedList;
+use std::{error::Error, process::Command};
 
-fn main() {
-    let shell = "sh -c";
-    let pager = std::env::var("PEA_PAGER")
-        .unwrap_or_else(|_| std::env::var("PAGER").unwrap_or_else(|_| "less".to_string()));
+use config::Config;
+
+mod config;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut config = Config::load()?.with_pager();
+
+    if config.pager.is_none() {
+        eprintln!("No pager found. Please set PAGER or PEA_PAGER environment variable.");
+        std::process::exit(1);
+    }
 
     let command = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
-    
+
     if command.is_empty() {
         eprintln!("Usage: pea <command> [args...]");
         std::process::exit(1);
     }
 
-    println!("{} '{} | {}'", shell, command, pager);
+    
+    let piped_commands = format!("{} | {}", command, config.pager.as_ref().unwrap());
+    config.format_shell_args(&piped_commands);
+
+    let status = Command::new(&config.shell)
+        .args(&config.shell_args)
+        .stdout(std::process::Stdio::inherit())
+        .status()?;    
+
+    std::process::exit(status.code().unwrap_or(1));
 }
