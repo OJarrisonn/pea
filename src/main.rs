83a98792da31fcfab4bf63a9bc181cd312b3dd80
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-08-22 13:38:34 -0300

    feat: execute a simple command
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::collections::LinkedList;
+
 fn main() {
-    println!("Hello, world!");
+    let shell = "sh -c";
+    let pager = std::env::var("PEA_PAGER")
+        .unwrap_or_else(|_| std::env::var("PAGER").unwrap_or_else(|_| "less".to_string()));
+
+    let command = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
+    
+    if command.is_empty() {
+        eprintln!("Usage: pea <command> [args...]");
+        std::process::exit(1);
+    }
+
+    println!("{} '{} | {}'", shell, command, pager);
 }
